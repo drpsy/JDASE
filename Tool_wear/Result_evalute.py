9ba2498a663e0ecad1c74a2d9072b9dbd164dd84
ZhangTeng <60456877+ZhangTeng-Hust@users.noreply.github.com>
2021-09-14 10:54:48 +0800

    Add files via upload

---
+++ Tool_wear/Result_evalute.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 14:11,2021/09/13
+@author: ZhangTeng
+"""
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+def predict(y_true,y_pred):
+    '''
+    y_true: 实际标签
+    y_pred: 预测标签
+    return: 返回四个评价指标所组成的array
+    '''
+
+    MAE = mean_absolute_error(y_true,y_pred)
+    MAPE = np.mean(np.abs((y_true - y_pred) / y_true))
+    MSE = mean_squared_error(y_true,y_pred)
+    R2 = r2_score(y_true,y_pred)
+    print("MAE  {}".format(mean_absolute_error(y_true,y_pred)))
+    print("MAPE {}".format(np.mean(np.abs((y_true - y_pred) / y_true))))
+    print("MSE  {}".format(mean_squared_error(y_true,y_pred)))
+    print("R2   {}".format(r2_score(y_true,y_pred)))
+    result = np.array([MAE,MAPE,MSE,R2])
+    return result
